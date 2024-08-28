f9b65dd0445471a4883670900422cad1d1c3b37f
luuhaohiep1218 <luuhaohiep8a@gmail.com>
2024-08-28 23:09:42 +0700

    Update data from website

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
+const methodOverride = require("method-override");
 const handlebars = require("express-handlebars");
 const { engine } = handlebars;
 const app = express();
@@ -21,6 +22,8 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride("_method"));
+
 // HTTP logger
 app.use(morgan("combined"));
 
@@ -29,7 +32,10 @@ app.engine(
   "hbs",
   engine({
     extname: ".hbs",
-  })
+    helpers: {
+      sum: (a, b) => a + b,
+    },
+  }),
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources", "views"));
